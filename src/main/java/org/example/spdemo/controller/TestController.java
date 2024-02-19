a7ec5a87ace6d30c06e025f92defc57734e203d2
Moonya <382510118@qq.com>
2024-02-19 14:11:46 +0800

    2-8 config & default value

--- src/main/java/org/example/spdemo/controller/TestController.java
+++ src/main/java/org/example/spdemo/controller/TestController.java
@@ -1,5 +1,6 @@
 package org.example.spdemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:ooo}")
+    private String testHello;
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello Moonya!!";
+        return "hello Moonya!!" + testHello;
     }
 
     @PostMapping("/hello/post")
