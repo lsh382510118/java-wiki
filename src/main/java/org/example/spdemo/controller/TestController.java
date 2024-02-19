09d626cdc99a119b7acd26fc992c9947052f997a
Moonya <382510118@qq.com>
2024-02-19 13:57:42 +0800

    2-6 add post api & unit test & http client

--- src/main/java/org/example/spdemo/controller/TestController.java
+++ src/main/java/org/example/spdemo/controller/TestController.java
@@ -1,14 +1,20 @@
 package org.example.spdemo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "hello Moonya!!";
+    }
+
+    @PostMapping("/hello/post")
+    public String hellPost(String name) {
+        return "hello " + name;
     }
 }
